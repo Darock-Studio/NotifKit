3c866ddb7a4cf90d5ef4c81fc9eb7c1c4147bde3
WindowsMEMZ <memz1@darock.top>
2025-01-13 01:22:28 +0800

    Fix package description placeholder

--- Package.swift
+++ Package.swift
@@ -12,6 +12,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .binaryTarget(name: "NotifKit", path: <#T##String#>)
+        .binaryTarget(name: "NotifKit", path: "NotifKit.xcframework")
     ]
 )
